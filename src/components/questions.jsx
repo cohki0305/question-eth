6ee5ba3b676ace7f457879a8b2709e623512ecda
cohki0305 <cohki0305@gmail.com>
2018-04-29 15:58:57 +0900

    able to add new question

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -2,12 +2,39 @@ import React from 'react'
 import Question from './question'
 import { connect } from 'react-redux'
 import factoryContract from '../contracts/question_factory'
+import getAccounts from '../eth_accounts'
 
 class Questions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
   async componentDidMount() {
+    const questions = await this.getQuestions()
+    this.props.handleClick(questions)
+  }
+
+  async getQuestions() {
     const instance = await factoryContract.deployed()
     const questions = await instance.getQuestions.call()
+
+    return questions
+  }
+
+  async addQuestion() {
+    const instance = await factoryContract.deployed()
+    const accounts = await getAccounts()
+    await instance.createQuestion(this.state.value, {from: accounts[0], gas: 1000000 })
+
+    const questions = await this.getQuestions()
     this.props.handleClick(questions)
+
+    this.setState({value: ''})
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
   render() {
@@ -16,6 +43,9 @@ class Questions extends React.Component {
         { this.props.questions.map((question, index) => {
           return <Question address={question} key={index} />
         }) }
+
+        <input type="text" value={this.state.value}  onChange={(e) => { this.handleChange(e) } }/>
+        <input type="submit" value="Submit" onClick={ () => { this.addQuestion() } }/>
       </div>
     )
   }
